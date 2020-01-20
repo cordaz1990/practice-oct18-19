810718e9943494afa99f0ca7e6c41d9204047f23
Cynthia Ordaz <cordaz@dons.usfca.edu>
2020-01-20 14:48:44 -0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,3 +1,23 @@
-print("hello")
-print("world")
-print("hello")
+from typing import TextIO
+from io import StringIO
+
+def process_file(reader: TextIO) -> int:
+    """ Read and process reader, which must start with a time_series header. Return the largest value after the header.
+    There may be multiple pieces of data on each line.
+    
+    >>> infile = StringIO('Example\\n 20. 3.\\n')
+    >>> process_file(infile)
+    20
+    >>> infile = StringIO('Example\\n 20. 3.\\n 100. 17. 15.\\n')
+    >>> process_file(infile)
+    100
+    """
+    #Read the description line
+    line = reader.readline()
+    
+    #Find the first non-comment line
+    line = reader.readline()
+    while line.startswith('#'):
+      line = reader.readline()
+      
+      
